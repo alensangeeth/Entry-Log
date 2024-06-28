6d320657c9ffad71ab94f0667839bea84806c9f8
alensangeeth <alenksangeeth2004@gmail.com>
2024-06-28 14:09:59 +0530

    second commit

---
+++ app/src/main/java/com/example/entrylog/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.entrylog;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText ed1,ed2;
+    AppCompatButton b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ed1=(EditText) findViewById(R.id.uname);
+        ed2=(EditText) findViewById(R.id.pass);
+        b1=(AppCompatButton) findViewById(R.id.login);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String getuname=ed1.getText().toString();
+                String getpass=ed2.getText().toString();
+                if(getuname.equals("admin")&&getpass.equals("12345"))
+                {
+                    Toast.makeText(getApplicationContext(),"SUCCESSFULLY LOGGED IN",Toast.LENGTH_SHORT).show();
+                    Intent i=new Intent(getApplicationContext(),Log.class);
+                    startActivity(i);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Invalid Credential", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+}
